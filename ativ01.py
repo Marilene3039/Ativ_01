a447efd51847c9325e3100452b971c813232025b
Marilene Martins <maaribadaras@gmail.com>
2023-10-04 19:41:50 -0300

    corrigindo código

--- ativ01.py
+++ ativ01.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 # Defina a rota raiz ("/") e a função que será executada quando a rota for acessada
 @app.route("/")
 def homepage():
-    return "<b1> Atividade 1 git: Marilene Martins Ramos<b/1>"
+    return "<h1> Atividade 1 git:</h1> <p>Marilene Martins Ramos</p>"
     
 
 
