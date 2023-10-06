d6c0503e422b5c418292bbb18291780788099247
Marilene Martins <maaribadaras@gmail.com>
2023-10-05 22:19:09 -0300

    add atividade 02

---
+++ Ativ02/atv_sqlite3.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+#Conectar ao banco de dados(ou criar se não existir)
+conn = sqlite3.connect('user.db')
+
+#Criar um cursor
+cursor = conn.cursor()
+
+#Criar uma tabela
+cursor.execute('''
+            CREATE TABLE IF NOT
+            EXISTS Usuarios (
+               id INTEGER PRIMARY KEY,
+               nome TEXT, 
+               idade INTEGER
+            )
+''')
+
+#Inserir dados
+cursor.execute('INSERT INTO Usuarios (nome,idade) VALUES (?, ?)', ('John', 12))
+cursor.execute('INSERT INTO Usuarios (nome, idade)VALUES (?, ?)', ('Renan', 20))
+
+#Commit para salvar as alterações
+conn.commit()
+
+#Consultar dados
+cursor.execute ('SELECT * FROM Usuarios')
+for row in cursor.fetchall():
+    print(row)
+
+    #Fechar a conexão
+    conn.close()
